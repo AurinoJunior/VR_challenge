60b6d489534004a350f65d28451c6c07341824d0
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-04 17:02:52 -0300

    Create field spec

---
+++ src/components/atoms/Field/Field.spec.tsx
@@ -0,0 +1,93 @@
+import "@testing-library/jest-dom";
+import { vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Field } from ".";
+
+const mockValue = "test";
+const setMockValue = vi.fn();
+
+describe("Field", () => {
+  it("should render field", () => {
+    render(
+      <Field
+        label="test"
+        name="test"
+        valueField={mockValue}
+        getValueField={setMockValue}
+      />
+    );
+
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("should call setMockValue with new value", () => {
+    render(
+      <Field
+        label="test"
+        name="test"
+        valueField={mockValue}
+        getValueField={setMockValue}
+      />
+    );
+
+    const inputElement = screen.getByRole("textbox");
+
+    fireEvent.change(inputElement, { target: { value: "Testando" } });
+
+    expect(setMockValue).toHaveBeenCalledTimes(1);
+  });
+
+  it("should render secret field", () => {
+    render(
+      <Field
+        label="test"
+        name="test"
+        valueField={mockValue}
+        getValueField={setMockValue}
+        isSecretField
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox", { name: /mostrar senha/i });
+
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it("should show password when click checkbox button", () => {
+    render(
+      <Field
+        label="test"
+        name="test"
+        valueField={mockValue}
+        getValueField={setMockValue}
+        isSecretField
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox", { name: /mostrar senha/i });
+    fireEvent.click(checkbox);
+
+    const input = screen.getByLabelText(/test/i);
+
+    expect(input).toHaveAttribute("type", "text");
+  });
+
+  it("should hide password when click checkbox button", () => {
+    render(
+      <Field
+        label="test"
+        name="test"
+        valueField={mockValue}
+        getValueField={setMockValue}
+        isSecretField
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox", { name: /mostrar senha/i });
+    fireEvent.click(checkbox);
+    fireEvent.click(checkbox);
+    const input = screen.getByLabelText(/test/i);
+
+    expect(input).toHaveAttribute("type", "password");
+  });
+});
