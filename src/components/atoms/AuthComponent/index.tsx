be31e0fc0d6e54ae24cd32dcb338d0a70188f779
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-07 13:52:01 -0300

    Add jwt-decode

--- src/components/atoms/AuthComponent/index.tsx
+++ src/components/atoms/AuthComponent/index.tsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
 import { Login } from "../../pages";
 
 interface IAuthComponentProps {
   children: React.ReactNode | string;
 }
 
-export function AuthComponent({ children }: IAuthComponentProps) {
-  const token = localStorage.getItem("vr-case@user");
+interface IResponseLoginApi {
+  email: string;
+  firstName: string;
+  gender: string;
+  id: number;
+  image: string;
+  lastName: string;
+  token: string;
+  username: string;
+}
 
+export function AuthComponent({ children }: IAuthComponentProps) {
+  const { id } = useParams();
+
+  const token = localStorage.getItem("vr-case@user");
   if (!token) {
+    return <Login />;
+  }
+
+  const decodeToken = jwt_decode<IResponseLoginApi>(token);
+
+  if (Number(id) !== decodeToken.id) {
     return <Login />;
   }
 
